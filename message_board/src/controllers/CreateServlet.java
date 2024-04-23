fe60d6050932de3b54601f69aed70041b9ad8277
eisuke.nakamura.m706@dirbato.co.jp <EisukeNakamura()@Dirbato-1423x>
2024-04-23 11:56:06 +0900

    ShowServlet.javaを作成

--- message_board/src/controllers/CreateServlet.java
+++ message_board/src/controllers/CreateServlet.java
@@ -1,79 +1,51 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CreateServlet
- */
+import models.Message;
+import utils.DBUtil;
+
 @WebServlet("/create")
 public class CreateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
     public CreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-    package controllers;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if (_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+            em.getTransaction().begin();
 
-    import java.io.IOException;
-    import java.sql.Timestamp;
+            Message m = new Message();
 
-    import javax.persistence.EntityManager;
-    import javax.servlet.ServletException;
-    import javax.servlet.annotation.WebServlet;
-    import javax.servlet.http.HttpServlet;
-    import javax.servlet.http.HttpServletRequest;
-    import javax.servlet.http.HttpServletResponse;
+            String title = request.getParameter("title");
+            m.setTitle(title);
 
-    import models.Message;
-    import utils.DBUtil;
+            String content = request.getParameter("content");
+            m.setContent(content);
 
-    @WebServlet("/create")
-    public class CreateServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setCreated_at(currentTime);
+            m.setUpdated_at(currentTime);
 
-        public CreateServlet() {
-            super();
+            em.persist(m);
+            em.getTransaction().commit();
+            em.close();
+
+            response.sendRedirect(request.getContextPath() + "/index");
         }
-
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            String _token = request.getParameter("_token");
-            if (_token != null && _token.equals(request.getSession().getId())) {
-                EntityManager em = DBUtil.createEntityManager();
-                em.getTransaction().begin();
-
-                Message m = new Message();
-
-                String title = request.getParameter("title");
-                m.setTitle(title);
-
-                String content = request.getParameter("content");
-                m.setContent(content);
-
-                Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-                m.setCreated_at(currentTime);
-                m.setUpdated_at(currentTime);
-
-                em.persist(m);
-                em.getTransaction().commit();
-                em.close();
-
-                response.sendRedirect(request.getContextPath() + "/index");
-            }
-        }
-
     }
+
 }
